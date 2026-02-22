2c700a0ebee1d80cb0de37ed8e8d1ed7618c48bc
EstebanCarrasco <est.carrasco@duocuc.cl>
2026-02-22 19:07:50 -0300

    fix:falta de uimetadata en usegame

--- src/composables/useGame.ts
+++ src/composables/useGame.ts
@@ -47,7 +47,11 @@ const gameState = ref<RoomState>({
         resultsEndsAt: null
     },
     stoppedBy: null,
-    gameOverReason: undefined
+    gameOverReason: undefined,
+    uiMetadata: {
+        activeView: 'LOBBY',
+        showTimer: false
+    }
 });
 
 export function useGame() {
@@ -354,7 +358,11 @@ export function useGame() {
                 resultsEndsAt: null
             },
             stoppedBy: null,
-            gameOverReason: undefined
+            gameOverReason: undefined,
+            uiMetadata: {
+                activeView: 'LOBBY',
+                showTimer: false
+            }
         };
 
         // 4. Clear URL
